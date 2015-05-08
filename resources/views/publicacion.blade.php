69506428ad2c942609055b38bf4f5ba0248bb759
Miguel <miguelruizbas@gmail.com>
2015-05-08 14:04:43 +0200

    noticias

--- resources/views/publicacion.blade.php
+++ resources/views/publicacion.blade.php
@@ -1,5 +1,9 @@
 @extends('master')
 
+@section('css')
+    <link rel="stylesheet" href="{{ URL::asset('css/jssocials.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/jssocials-theme-classic.css')}}">
+    @endsection
 
 @section('anuncio')
     @include('extras.anuncio')
@@ -30,16 +34,35 @@
 
 
                 <div class="row" >
-                    <div class="col col-xs-5 col-sm-5 col-md-2 col-lg-2">
-                        <img src = "{{ URL::asset($articulo[0]->url_imagen)}} " alt="Responsive image" width='110' height="110">
+
+                    <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                        <div class="row">
+
+                            <div class="col col-xs-10 col-sm-10 col-md-10 col-lg-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
+                                <h1 style="font-size: 3em" id="tituloNotica"> {{ $articulo[0]->titulo }}</h1>
+                                <br>
+                                <div id="share" class="jssocials"><div class="jssocials-shares"><div class="jssocials-share jssocials-share-twitter"><a class="jssocials-share-link" href="https://twitter.com/share?url=http%3A%2F%2Flocalhost%2Flara%2Fpublic%2Ffranquicias-de-hola%2520que%2520tal%2FHuerta%2520de%2520Casillas%2520e%2520Hija&amp;text=Multifranquicias" target="_blank"><i class="fa fa-twitter jssocials-share-logo"></i><span class="jssocials-share-label">Tweet</span></a><div class="jssocials-share-count-box jssocials-share-no-count"><span class="jssocials-share-count"></span></div></div><div class="jssocials-share jssocials-share-facebook"><a class="jssocials-share-link" href="https://facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%2Flara%2Fpublic%2Ffranquicias-de-hola%2520que%2520tal%2FHuerta%2520de%2520Casillas%2520e%2520Hija" target="_blank"><i class="fa fa-facebook jssocials-share-logo"></i><span class="jssocials-share-label">Like</span></a><div class="jssocials-share-count-box jssocials-share-no-count"><span class="jssocials-share-count"></span></div></div><div class="jssocials-share jssocials-share-googleplus"><a class="jssocials-share-link" href="https://plus.google.com/share?url=http%3A%2F%2Flocalhost%2Flara%2Fpublic%2Ffranquicias-de-hola%2520que%2520tal%2FHuerta%2520de%2520Casillas%2520e%2520Hija" target="_blank"><i class="fa fa-google-plus jssocials-share-logo"></i><span class="jssocials-share-label">+1</span></a><div class="jssocials-share-count-box jssocials-share-no-count"><span class="jssocials-share-count"></span></div></div><div class="jssocials-share jssocials-share-linkedin"><a class="jssocials-share-link" href="https://www.linkedin.com/shareArticle?url=http%3A%2F%2Flocalhost%2Flara%2Fpublic%2Ffranquicias-de-hola%2520que%2520tal%2FHuerta%2520de%2520Casillas%2520e%2520Hija" target="_blank"><i class="fa fa-linkedin jssocials-share-logo"></i><span class="jssocials-share-label">Share</span></a><div class="jssocials-share-count-box jssocials-share-no-count"><span class="jssocials-share-count"></span></div></div><div class="jssocials-share jssocials-share-pinterest"><a class="jssocials-share-link" href="https://pinterest.com/pin/create/bookmarklet/?&amp;url=http%3A%2F%2Flocalhost%2Flara%2Fpublic%2Ffranquicias-de-hola%2520que%2520tal%2FHuerta%2520de%2520Casillas%2520e%2520Hija&amp;description=Multifranquicias" target="_blank"><i class="fa fa-pinterest jssocials-share-logo"></i><span class="jssocials-share-label">Pin it</span></a><div class="jssocials-share-count-box jssocials-share-no-count"><span class="jssocials-share-count"></span></div></div></div></div>
+<br>
+                            </div>
+
+                        </div>
+                    <div class="row">
+
+                        <div class="col col-xs-10 col-sm-10 col-md-10 col-lg-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
+
+
+
+                            <div><img style="float: left; margin-right: 1%;" src="{{ URL::asset($articulo[0]->url_imagen)}}" alt="Responsive image" width='200' height="200" />  {!! $articulo[0]->contenido !!} </div>
+                        <p class="fecha_publicacion pull-right">{{ '21-02-2012' }}</p>
+                            </div>
+
                     </div>
 
-                    <div class="col col-xs-7 col-sm-7 col-md-10 col-lg-10">
-                        <h3 id="tituloNotica"> {{ $articulo[0]->titulo }}</h3>
-                        <br>
-                        <p  id="textoNoticia"> {!! $articulo[0]->contenido !!}</p>
-                        <p class="fecha_publicacion pull-right">{{ '21-02-2012' }}</p>
+
                     </div>
+
+
                 </div>
                 <hr>
 
@@ -56,3 +79,19 @@
     @include('extras.derecha')
 
 @endsection
+
+@section('javascript')
+    <script type="text/javascript" src="{{ URL::asset('js/jssocials.min.js') }}"></script>
+
+@endsection
+@section('ready')
+
+    $("#share").jsSocials({
+    shares: ["twitter", "facebook", "googleplus", "linkedin", "pinterest"],
+    url: "{{Request::url()}}",
+    showLabel:true,
+    showCount:true
+    });
+
+
+    @endsection
