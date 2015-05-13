0e6b7fbce72a51e55a2d8246b493c9e7ad909b9e
juanca <juankaw89@gmail.com>
2015-05-13 10:41:42 +0200

    Commit 20-04-2015

--- public/area_privada/js/dropzone/dropzone.js
+++ public/area_privada/js/dropzone/dropzone.js
@@ -141,7 +141,7 @@
             dictDefaultMessage: "Drop files here to upload",
             dictFallbackMessage: "Your browser does not support drag'n'drop file uploads.",
             dictFallbackText: "Please use the fallback form below to upload your files like in the olden days.",
-            dictFileTooBig: "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
+            dictFileTooBig: "Archivo demasiado grande ({{filesize}}MiB). Máx tamaño: {{maxFilesize}}MiB.",
             dictInvalidFileType: "You can't upload files of this type.",
             dictResponseError: "Server responded with {{statusCode}} code.",
             dictCancelUpload: "Cancel upload",
