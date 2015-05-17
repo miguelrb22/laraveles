34aca16c3f943a408f5201e5c0f66709e9314bc8
Miguel <miguelruizbas@gmail.com>
2015-05-17 12:22:22 +0200

    pruebas

--- public/carousel.blade.php
+++ public/carousel.blade.php
@@ -1,4 +1,6 @@
 <div id="myCarousel" class="carousel slide" data-ride="carousel" style="">
+
+
   <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
