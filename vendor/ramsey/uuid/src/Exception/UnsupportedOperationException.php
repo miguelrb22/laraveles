e601489d96b5c7436cf4de6473af8d5938511828
Miguel <miguelruizbas@gmail.com>
2015-05-17 11:35:15 +0200

    AÑADIDA CARPETA VENDOR

---
+++ vendor/ramsey/uuid/src/Exception/UnsupportedOperationException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the Ramsey\Uuid library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) 2012-2014 Ben Ramsey <http://benramsey.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Ramsey\Uuid\Exception;
+
+/**
+ * Thrown to indicate that the requested operation is not supported.
+ */
+class UnsupportedOperationException extends \RuntimeException
+{
+}
