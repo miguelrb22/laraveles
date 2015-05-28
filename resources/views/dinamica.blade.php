669b4f8a5b22020aada219ab8abc465da482a93a
juanca <juankaw89@gmail.com>
2015-05-28 13:35:53 +0200

    mensaje de toda la vida

--- resources/views/dinamica.blade.php
+++ resources/views/dinamica.blade.php
@@ -54,11 +54,11 @@
                         @foreach($franquicias as $franquicia)
                         <div class="row">
                             <div class="col col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                                <a href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',$franquicia->nombre."/".$franquicia->nombre_comercial)))}}"><img class="img-rounded img-responsive f-logo" src="{{ asset($franquicia->logo_url) }}"></a>
+                                <a href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',$franquicia->nombre."/".strtr(utf8_decode($franquicia->nombre_comercial),utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'))))}}"><img class="img-rounded img-responsive f-logo" src="{{ asset($franquicia->logo_url) }}"></a>
                             </div>
                             <div class="col col-xs-4 col-sm-4 col-md-8 col-lg-8 f-desc">
                                 <p>
-                                    <a href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',$franquicia->nombre."/".$franquicia->nombre_comercial)))}}"><h3>{{$franquicia->nombre_comercial}}</h3></a>
+                                    <a href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',$franquicia->nombre."/".strtr(utf8_decode($franquicia->nombre_comercial),utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'))))}}"><h3>{{$franquicia->nombre_comercial}}</h3></a>
                                     <p>{{ substr(strip_tags($franquicia->descripcion),0,250)."..." }}</p>
                                 </p>
                             </div>
