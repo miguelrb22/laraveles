934bdce54bf087283ab626e4b2f9579a64cbb209
juanca <juankaw89@gmail.com>
2015-05-28 13:51:12 +0200

    mensaje de toda la vida

--- resources/views/dinamica_subcategorias.blade.php
+++ resources/views/dinamica_subcategorias.blade.php
@@ -57,7 +57,7 @@
                             @for($i=0 ; $i< count($franquicias); $i++)
                                 <div class="row">
                                     <div class="col col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                                        <a href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',$franquicias[$i]->nombre."/".strtr(utf8_decode($franquicias[$i]->nombre_comercial),utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'))))}}"><img class="img-rounded img-responsive f-logo" src="{{ asset($franquicias[$i]->logo_url) }}"></a>
+                                        <a href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',strtr(utf8_decode($franquicias[$i]->nombre."/".$franquicias[$i]->nombre_comercial),utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'))))}}"><img class="img-rounded img-responsive f-logo" src="{{ asset($franquicias[$i]->logo_url) }}"></a>
                                     </div>
                                     <div class="col col-xs-4 col-sm-4 col-md-8 col-lg-8 f-desc">
                                         <p>
