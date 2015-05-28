5748f932523245e71083f364e051652226501b0d
juanca <juankaw89@gmail.com>
2015-05-28 13:40:11 +0200

    mensaje de toda la vida

--- resources/views/franquicias.blade.php
+++ resources/views/franquicias.blade.php
@@ -63,7 +63,8 @@
                                                             <a href="{{URL::to('franquicias-de-'.strtr(utf8_decode(strtolower(str_replace(' ','-',$lista[$i]))),utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),
                                                                         'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'))}}" class="list-group-item small"> {{$lista[$i]}} </a>
                                                             @for($j=1 ; $j < count($lista[$i+1]); $j++)
-                                                                <a href="{{URL::to('franquicias-de-'.$lista[$i+1][$j])}}" class="list-group-item small"> {{$lista[$i+1][$j]}} </a>
+                                                                <a href="{{URL::to('franquicias-de-'.strtr(utf8_decode(strtolower(str_replace(' ','-',$lista[$i+1][$j]))),utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),
+                                                                        'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'))}}" class="list-group-item small"> {{$lista[$i+1][$j]}} </a>
                                                             @endfor
                                                         </div>
                                                     </div>
