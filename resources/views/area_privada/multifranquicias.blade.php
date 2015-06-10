54414e156353c427245f50691a39352f736477b5
juanca <juankaw89@gmail.com>
2015-06-10 11:05:57 +0200

    mensaje de toda la vida

--- resources/views/area_privada/multifranquicias.blade.php
+++ resources/views/area_privada/multifranquicias.blade.php
@@ -1303,6 +1303,12 @@
 
                 });
 
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
                 $.ajax({
 
                     type: "POST",
