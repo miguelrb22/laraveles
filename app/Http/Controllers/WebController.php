417060ee803678e603bb37611ea65480a9e9e639
juanca <juankaw89@gmail.com>
2015-06-16 11:48:18 +0200

    New changelist

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -781,7 +781,7 @@ class WebController extends Controller {
 
         //Obtenemos la franquicia por el nombre pasado desde el routes.
         //$franquicia = Franquicia::where('nombre_comercial', '=', $nombre)->firstOrFail();
-        $franquicia =  Franquicia::select(DB::raw("select * from franquicia where Replace(nombre_comercial, 'ñ', 'n') = ". "'".$nombre."'"));
+        $franquicia =  Franquicia::select(DB::raw("select * from franquicia where Replace(nombre_comercial, 'ñ', 'n') = ". "'".$nombre."'"))->get();
 
 
         dd($franquicia);
