b11d93c2a8e6664d55fb67e9bbc03580c675da5c
juanca <juankaw89@gmail.com>
2015-06-16 10:32:25 +0200

    New changelist

--- resources/views/resultados_lista.blade.php
+++ resources/views/resultados_lista.blade.php
@@ -61,7 +61,7 @@
                                 <div class="col col-xs-8 col-sm-8 col-md-8 col-lg-8 f-desc">
                                     <p>
                                         <!--<label class="pull-right badge badge-success">Inversión: {!! $franquicia->inversion !!}</label> -->
-                                        <a href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',$franquicia->nombre."/".strtr(utf8_decode($franquicia->nombre_comercial),utf8_decode('àáâãäçèéêëìíîïòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),'aaaaaceeeeiiiiooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'))))}}"><h3>{{$franquicia->nombre_comercial}}</h3></a>
+                                        <a href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',$franquicia->nombre."/".strtr(utf8_decode($franquicia->nombre_comercial),utf8_decode('àáâãäçèéêëìíîïòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝ'),'aaaaaceeeeiiiiooooouuuuyyAAAAACEEEEIIIIOOOOOUUUUY'))))}}"><h3>{{$franquicia->nombre_comercial}}</h3></a>
                                         <p>{{ substr(strip_tags($franquicia->descripcion),0,250)."..." }}</p>
                                         <label>Actividad : {{$franquicia->nombre}}</label>
                                     </p>
