f580422de6c9a6441c50fd191e2ced5a2ff3edd9
juanca <juankaw89@gmail.com>
2015-06-23 09:56:58 +0200

    New changelist

--- resources/views/extras/entrevistas.blade.php
+++ resources/views/extras/entrevistas.blade.php
@@ -1,10 +1,11 @@
+@if(!$entrevistas->isEmpty())
 <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
     <div class="row panel panel-info text-center">
         <div class="panel-heading textoblanco" style="background:#333">
             <i class="fa fa-video-camera textoblanco"></i> <span class="textoblanco">Entrevistas</span>
         </div>
         <div class="panel-body" style="margin-bottom: -16px;">
-            @if(!$entrevistas->isEmpty())
+
                 @foreach($entrevistas as $entrevista)
                     <div class="row">
                         <a href="{{ strtolower(str_replace(" ","-",URL::to('noticias/'.preg_replace("/[^a-zA-Z0-9\s\-]/","",$entrevista->titulo).'/'.$entrevista->id)))}}"> <h3>{{$entrevista->titulo}}</h3></a>
@@ -23,7 +24,7 @@
                     </div>
                     <hr>
                 @endforeach
-            @endif
         </div>
     </div>
 </div>
+@endif
