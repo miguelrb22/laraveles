a296681078e1786ec0a1cd502303cda272c8e83a
juanca <juankaw89@gmail.com>
2015-06-30 10:53:55 +0200

    New changelist

--- resources/views/extras/anuncio.blade.php
+++ resources/views/extras/anuncio.blade.php
@@ -2,7 +2,7 @@
 
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <a class="banner_sup"  title="perfil">
+            <a class="banner_sup" href="{{URL::to('franquicias-de-'.strtolower(str_replace(' ','-',strtr(utf8_decode($bannerSup[0]->nombre."/".$bannerSup[0]->nombre_comercial),utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'))))}}" title="perfil">
                 <img  onclick="estadisticas(25,'{{$bannerSup[0]->id}}');" id="publicidad" src="{{ asset($bannerSup[0]->url_imagen) }}" class="img-responsive" alt="Responsive image">
             </a>
         </div>
