9d21a5ce412d2ebc2b471e11e58ca3e8b7a2c931
juanca <juankaw89@gmail.com>
2015-07-13 14:18:08 +0200

    New changelist

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -304,7 +304,6 @@
 
                                 @if(!$publicaciones->isEmpty())
 
-                                    {{dd($publicaciones)}}
                                     <?php
                                         $meses = array ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
                                                         "Septiembre", "Octube", "Noviembre", "Diciembre");
