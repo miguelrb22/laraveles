f887bc10b2aa11b30ac0f61924c95801bcfe225c
juanca <juankaw89@gmail.com>
2015-07-13 14:41:53 +0200

    New changelist

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -338,11 +338,12 @@
 
                                                         if(strlen($minutos) < 2){$minutos = "0".$minutos;}
                                                         if(strlen($hora) < 2){$hora = "0".$hora;}
-                            dd("no peta");
                                                         $ffinal = $dias[$fecha->dayOfWeek-1]. " " . $fecha->day . " de " . $meses[$fecha->month-1] . " de " . $fecha->year . " " .
                                                                       $hora . ":" . $minutos;
 
-                                                        echo $ffinal;
+                                                    dd("no peta");
+
+                                                    echo $ffinal;
                                                     ?>
 
                                                 </p>
