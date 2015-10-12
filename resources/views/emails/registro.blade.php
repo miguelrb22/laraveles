fcb4fe94b475659b424844ed813edce02ae1656d
Miguel <miguelruizbas@gmail.com>
2015-10-12 13:17:38 +0200

    command

--- resources/views/emails/registro.blade.php
+++ resources/views/emails/registro.blade.php
@@ -2,9 +2,9 @@
 <head></head>
 <body>
 <h3> Nuevo usuario registrado</h3>
-<p>Una persona acaba de registrase como usuario en nuestro sistema. A continuación se muestran sus datos:</p>
+<p>Una persona acaba de registrase como usuario en nuestro sistema. A {{ utf8_encode('continuación') }} se muestran sus datos:</p>
 <p>Nombre de usuario: {{$username}}</p>
 <p>Email: {{$email}}</p>
-<p>Teléfono: {{$telefono}}</p>
+<p>{{ utf8_encode('Teléfono) }}: {{$telefono}}</p>
 </body>
 </html>
